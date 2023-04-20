0aa03a3c1bba79152f56d0766d9c23b5d8dc9e6a
fordashton3 <fordashton3@gmail.com>
2023-04-19 22:58:58 -0500

    Compressed all  objects into a single package

--- src/Objects/Utilities.java
+++ src/Objects/Utilities.java
@@ -5,8 +5,8 @@ import java.awt.*;
 public class Utilities extends Property{
 	private final Utility utility;
 	private final int houses;
-	public Utilities(int spaceNumber, int price, int rent, boolean isElectric, Utility utility) {
-		super(spaceNumber, price, rent, Color.white, 0);
+	public Utilities(String name, int spaceNumber, int price, int rent, boolean isElectric, Utility utility) {
+		super(name, spaceNumber, price, rent, Color.white, 0);
 		this.utility = utility;
 		houses = 0;
 	}
