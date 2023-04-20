df1626046414f1629ba7dbc296a61b8ae2ca1a1d
fordashton3 <fordashton3@gmail.com>
2023-04-19 23:00:24 -0500

    Compressed all  objects into a single package

--- src/Objects/Railroad.java
+++ src/Objects/Railroad.java
@@ -5,8 +5,8 @@ import java.awt.*;
 public class Railroad extends Property{
 	Direction direction;
 
-	public Railroad(int spaceNumber, int price, int rent, Direction direction) {
-		super(spaceNumber, price, rent, Color.white, 0);
+	public Railroad(String name, int spaceNumber, int price, int rent, Direction direction) {
+		super(name, spaceNumber, price, rent, Color.white, 0);
 		this.direction = direction;
 	}
 
