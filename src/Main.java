88c202c885809201883c99fb7b6a3241ec933711
fordashton3 <fordashton3@gmail.com>
2023-04-20 20:05:10 -0500

    Compressed all  objects into a single package

--- src/Main.java
+++ src/Main.java
@@ -24,16 +24,17 @@ public class Main extends Application {
 	private final IntegerProperty playerPosition = new SimpleIntegerProperty(0);
 
 	// Image paths
-	private static final String GO_IMAGE_PATH = "images/go.png";
-	private static final String JAIL_IMAGE_PATH = "images/jail.png";
-	private static final String FREE_PARKING_IMAGE_PATH = "images/free_parking.png";
-	private static final String GO_TO_JAIL_IMAGE_PATH = "images/go_to_jail.png";
-	private static final String COMMUNITY_CHEST_IMAGE_PATH = "images/community_chest.png";
-	private static final String CHANCE_IMAGE_PATH = "images/chance.png";
-	private static final String RAILROAD_IMAGE_PATH = "images/railroad.png";
-	private static final String TAX_IMAGE_PATH = "images/tax.png";
-	private static final String WATER_IMAGE_PATH = "images/water_works.png";
-	private static final String ELECTRIC_IMAGE_PATH = "images/electric_company.png";
+	private static final String GO_IMAGE_PATH = "images//go.png";
+	private static final String JAIL_IMAGE_PATH = "images//jail.png";
+	private static final String FREE_PARKING_IMAGE_PATH = "images//free_parking.png";
+	private static final String GO_TO_JAIL_IMAGE_PATH = "images//go_to_jail.png";
+	private static final String COMMUNITY_CHEST_IMAGE_PATH = "images//community_chest.png";
+	private static final String CHANCE_IMAGE_PATH = "images//chance.png";
+	private static final String RAILROAD_IMAGE_PATH = "images//railroad.png";
+	private static final String WATER_IMAGE_PATH = "images//water_works.png";
+	private static final String ELECTRIC_IMAGE_PATH = "images//electric_company.png";
+	private static final String INCOME_TAX = "images//income_tax.png";
+	private static final String LUXURY_TAX = "images//luxury_tax.png";
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -65,15 +66,15 @@ public class Main extends Application {
 		board.setGridLinesVisible(true);
 
 		// Create cells for the board
-		for (int row = 0; row < BOARD_SIZE; row++) {
-			for (int col = 0; col < BOARD_SIZE; col++) {
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			for (int x = 0; x < BOARD_SIZE; x++) {
 				// Create cell rectangle
 				Rectangle cell = new Rectangle(CELL_SIZE, CELL_SIZE);
 				cell.setFill(Color.WHITE);
 				cell.setStroke(Color.BLACK);
 
 				// Add text to cell
-				String text = "Cell " + (row * BOARD_SIZE + col + 1);
+				String text = "Cell " + (y * BOARD_SIZE + x + 1);
 				Label label = new Label(text);
 				label.setWrapText(true);
 				label.setMaxWidth(CELL_SIZE);
@@ -83,36 +84,50 @@ public class Main extends Application {
 
 				// Add image to specific cells
 				ImageView imageView = null;
-				if (row == 0 && col == 0) {
+				if (y == 0 && x == 0) {
 					imageView = createImageView(FREE_PARKING_IMAGE_PATH);
-				} else if (row == 0 && col == BOARD_SIZE - 1) {
-					imageView = createImageView(GO_TO_JAIL_IMAGE_PATH);
-				} else if (row == BOARD_SIZE - 1 && col == 0) {
-					imageView = createImageView(JAIL_IMAGE_PATH);
-				} else if (row == BOARD_SIZE - 1 && col == BOARD_SIZE - 1) {
-					imageView = createImageView(GO_IMAGE_PATH);
-				} else if (row == 0 && col == 2) {
-					imageView = createImageView(COMMUNITY_CHEST_IMAGE_PATH);
-				} else if (row == 0 && col == 7) {
-					imageView = createImageView(CHANCE_IMAGE_PATH);
-				} else if (row == 2 && col == 0) {
-					imageView = createImageView(RAILROAD_IMAGE_PATH);
-				} else if (row == 2 && col == 9) {
-					imageView = createImageView(RAILROAD_IMAGE_PATH);
-				} else if (row == 7 && col == 0) {
-					imageView = createImageView(WATER_IMAGE_PATH);
-				} else if (row == 7 && col == 9) {
-					imageView = createImageView(ELECTRIC_IMAGE_PATH);
-				} else if (row == BOARD_SIZE - 1 && col == 2) {
-					imageView = createImageView(TAX_IMAGE_PATH);
 				}
+				else if (y == 0 && x == BOARD_SIZE - 1) {
+					imageView = createImageView(GO_TO_JAIL_IMAGE_PATH);
+				}
+				else if (y == BOARD_SIZE - 1 && x == 0) {
+					imageView = createImageView(JAIL_IMAGE_PATH);
+				}
+				else if (y == BOARD_SIZE - 1 && x == BOARD_SIZE - 1) {
+					imageView = createImageView(GO_IMAGE_PATH);
+				}
+				else if (y == 3 && x == 0 || y == BOARD_SIZE - 1 && x == 8 || y == 3 && x == BOARD_SIZE - 1) {
+					imageView = createImageView(COMMUNITY_CHEST_IMAGE_PATH);
+				}
+				else if (y == 6 && x == BOARD_SIZE - 1 || y == 0 && x == 2 || y == BOARD_SIZE - 1 && x == 3) {
+					imageView = createImageView(CHANCE_IMAGE_PATH);
+				}
+				else if (y == 2 && x == 0) {
+					imageView = createImageView(RAILROAD_IMAGE_PATH);
+				}
+				else if (y == 2 && x == 9) {
+					imageView = createImageView(RAILROAD_IMAGE_PATH);
+				}
+				else if (y == 7 && x == 0) {
+					imageView = createImageView(WATER_IMAGE_PATH);
+				}
+				else if (y == 7 && x == 9) {
+					imageView = createImageView(ELECTRIC_IMAGE_PATH);
+				}
+				else if (y == BOARD_SIZE - 1 && x == 6){
+					imageView = createImageView(INCOME_TAX);
+				}
+				else if (y ==  5&& x == BOARD_SIZE - 1){
+					imageView = createImageView(LUXURY_TAX);
+				}
+
 
 				// Add cell contents to the board
 				if (imageView != null) {
 					StackPane cellStackPane = new StackPane(cellContents, imageView);
-					board.add(cellStackPane, col, row);
+					board.add(cellStackPane, x, y);
 				} else {
-					board.add(cellContents, col, row);
+					board.add(cellContents, x, y);
 				}
 			}
 		}
@@ -124,8 +139,8 @@ public class Main extends Application {
 	private ImageView createImageView(String imagePath) {
 		Image image = new Image(imagePath);
 		ImageView imageView = new ImageView(image);
-		imageView.setFitHeight(CELL_SIZE * 0.8);
-		imageView.setFitWidth(CELL_SIZE * 0.8);
+		imageView.setFitHeight(CELL_SIZE * 0.6);
+		imageView.setFitWidth(CELL_SIZE * 0.6);
 		return imageView;
 	}
 
